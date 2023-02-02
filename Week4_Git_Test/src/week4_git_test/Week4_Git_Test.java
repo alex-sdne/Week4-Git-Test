acd58237abd577b3dc4deb3a3efa7a40443e913a
Alex Kalicharan <kalichaa@sheridancollege.ca>
2023-02-02 10:07:12 -0500

    first commit

---
+++ Week4_Git_Test/src/week4_git_test/Week4_Git_Test.java
@@ -0,0 +1,17 @@
+package week4_git_test;
+
+/**
+ * SYST17796 Fundamentals of Software Design and Development
+ *
+ * @author Alex Kalicharan
+ */
+public class Week4_Git_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Testing week 4 git");
+    }
+
+}
